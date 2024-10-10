1ca60ca97b5d9ec3c97630c1c4869493a0d739ff
Max Yeap <max@yongtuck.com>
2024-10-11 00:48:46 +0800

    Implement devise for user authentication

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  include RackSessionsFix
+  respond_to :json
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def respond_with(current_user, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: {code: 200, message: 'Signed up successfully.'},
+        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+      }
+    else
+      render json: {
+        status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
+      }, status: :unprocessable_entity
+    end
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
+  end
+end
