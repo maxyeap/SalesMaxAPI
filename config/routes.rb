5ae291f452a301c162270e5ab108986f4b89ce76
Max Yeap <max@yongtuck.com>
2024-10-29 22:35:10 +0800

    Add route for Deal controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :deals
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -16,4 +17,5 @@ Rails.application.routes.draw do
 
   # lead routes
   resources :leads, only: [:index, :create, :show, :destroy, :update]
+  resources :deals
 end
