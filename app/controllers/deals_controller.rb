3df5a1273b00387d0904b6d09b3ff087e155bb76
Max Yeap <max@yongtuck.com>
2024-10-29 22:41:03 +0800

    Add success message for the methods

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -4,13 +4,18 @@ class DealsController < ApplicationController
   # GET /deals
   def index
     @deals = Deal.all
-
-    render json: @deals
+    render json: { success: true, data: @deals }
+  rescue => e
+    render json: { success: false, message: e.message }, status: :unprocessable_entity
   end
 
   # GET /deals/1
   def show
-    render json: @deal
+    if @deal
+      render json: { success: true, data: @deal }
+    else
+      render json: { success: false, message: 'Deal not found' }, status: :not_found
+    end
   end
 
   # POST /deals
@@ -18,25 +23,32 @@ class DealsController < ApplicationController
     @deal = Deal.new(deal_params)
 
     if @deal.save
-      render json: @deal, status: :created, location: @deal
+      render json: { success: true, data: @deal }, status: :created, location: @deal
     else
-      render json: @deal.errors, status: :unprocessable_entity
+      render json: { success: false, errors: @deal.errors }, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /deals/1
   def update
     if @deal.update(deal_params)
-      render json: @deal
+      render json: { success: true, data: @deal }
     else
-      render json: @deal.errors, status: :unprocessable_entity
+      render json: { success: false, errors: @deal.errors }, status: :unprocessable_entity
     end
   end
 
   # DELETE /deals/1
   def destroy
-    @deal.destroy
+    if @deal.destroy
+      render json: { success: true, message: 'Deal was successfully deleted.' }
+    else
+      render json: { success: false, message: 'Failed to delete the deal.' }, status: :unprocessable_entity
+    end
+  rescue => e
+    render json: { success: false, message: e.message }, status: :unprocessable_entity
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
